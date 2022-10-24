7c600646010c988503b3a6b55165eb60062dd84d
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-23 23:25:41 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -130,10 +130,15 @@ class Paths
 
 	inline static public function lua(key:String, ?library:String)
 	{
-		return getPath('$key.lua', TEXT, library);
+		return Main.path + getPath('$key.lua', TEXT, library);
 	}
 
-	static public function video(key:String)
+	inline static public function luaAsset(key:String, ?library:String)
+	{
+	return getPath('$key.lua', TEXT, library);
+	
+		
+		static public function video(key:String)
 	{
 		#if MODS_ALLOWED
 		var file:String = modsVideo(key);
